6872e85fd34228028b83a93087bcc23243e2cdcd
JackDevAU <jpr178@live.com.au>
2026-02-18 17:17:41 +1000

    feat: add tsup and move to esm only - fixes inquirer (esm only package)

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  target: 'node16',
+  shims: true,
+  clean: true,
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
+});
